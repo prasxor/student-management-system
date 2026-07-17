4c46576337a43d75a26ff2eb5506f4f666e9850b
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-07-17 21:53:08 +0530

    refactor : condition and added multi-user data store in dictionary

--- main.py
+++ main.py
@@ -2,47 +2,50 @@ students = dict()
 
 print('''Display Menu
 
-1. Add Student
-2. View All Students
-3. Search Student
-4. Update Student
-5. Delete Student
-6. Save to File (Optional)
-7. Load from File (Optional)
-8. Exit
-''')
+while True:
+    try:
+        userChoice = int(input("Enter your choice : "))
 
-try:
-    userChoice = int(input("Enter your choice : "))
-    
-    if userChoice == 1:
-        while True:
-            stdId = int(input("Enter your student ID: "))
-            stdName = str(input("Enter your Name: "))
-            stdAge = int(input("Enter your Age: "))
-            stdMarks = int(input("Enter your Marks: "))
-            print("Confirm your details")
-            print(f'''
-                Student Id : {stdId}
-                Student Name : {stdName}
-                Student Age : {stdAge}
-                Student Marks : {stdMarks}''')
-            print('''Press "0" to Confirm''')
-            print('''Press "1" to Edit''')
-            userConfirm = int(input("Enter your Choice: "))
-            if userConfirm == 0:
-                print("Your details are Confirmed")
-                break
-        if students.get("stdId") == stdId and students.get("stdName")== stdName and students.get('StdAge')==stdAge and students.get("stdMarks")==stdMarks:
-            print("You're record is already existed !")
-        else:
-            print("We found that you're not registered, we are creating your account :)")
-            students["stdId"] = stdId
-            students["stdName"] = stdName
-            students["stdAge"] = stdAge
-            students["stdMarks"] = stdMarks
-            print("we created your account")
+        if userChoice == 1:
+            while True:
+                stdId = int(input("Enter your student ID: "))
+                stdName = str(input("Enter your Name: "))
+                stdAge = int(input("Enter your Age: "))
+                stdMarks = int(input("Enter your Marks: "))
+                print("Confirm your details")
+                print(f'''
+                    Student Id : {stdId}
+                    Student Name : {stdName}
+                    Student Age : {stdAge}
+                    Student Marks : {stdMarks}''')
+                print('''Press "0" to Confirm''')
+                print('''Press "1" to Edit''')
+                userConfirm = int(input("Enter your Choice: "))
+                if userConfirm == 0:
+                    print("Your details are Confirmed")
+                    break
+            if students["stdId"] == stdId:
+                print("You're record is already existed !")
+            else:
+                print("We found that you're not registered, we are creating your account :)")
+                students[stdId] = {
+                    "name": stdName,
+                    "age": stdAge,
+                    "marks": stdMarks
+                }
+                print("we created your account")
+                print(students)
+        elif userChoice == 2:
             print(students)
-            
-except:
-    print("Please enter number from given index !")
+        elif userChoice == 3:
+            print("Enter your details to get your record")
+            chckId = int(input("Enter your student ID: "))
+            chckName = str(input("Enter your Name: "))
+            if students.get("stdId")== chckName and students.get("stdName")==chckName:
+                print("You're record is already existed !")
+                print(f'''(
+                
+                )''')
+
+    except:
+        print("Please enter number from given index !")
