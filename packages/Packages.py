c8d2eb7e74491f3f4fd9242ef9abd77724805d88
Prashant kumar sinha <116288719+prasxor@users.noreply.github.com>
2026-07-20 22:40:12 +0530

    Extend Student class with additional fields and features
    Added gender, email, and course fields to the Student class. Refactored class attributes (passing_marks, student_count, college_name). Implemented display_student method to show comprehensive student information. Expanded menu system from 8 to 12 options, including passed/failed students filtering, topper display, and average marks calculation. Updated data collection flow (std_input) and add_student functions to handle new fields.

--- packages/Packages.py
+++ packages/Packages.py
@@ -1,15 +1,20 @@
 students = dict()
 
 class Student:
-    def __init__(self,std_id,name,age,marks, mobile):
+    def __init__(self,std_id,name,age,gender, mobile, email,course,marks):
+        self.stdId = std_id
         self.name = name
         self.age = age
-        self.marks = marks
-        self.stdId = std_id
+        self.gender = gender
         self.mobile = self.validate_mobile(mobile)
+        self.email = email
+        self.course = course
+        self.marks = marks
 
-    course = ["python full stack", "java full stack", "software testing"]
-    institute = "qspider"
+
+    passing_marks = 35
+    student_count = 0
+    college_name = "Acme degree college"
 
     @staticmethod
     def validate_mobile(self):
@@ -20,20 +25,44 @@ class Student:
 
 
 
+
+    @staticmethod
+    def display_student(self,curr_instance):
+        print(f'''
+        student Id   : {curr_instance.stdId}
+        student name : {curr_instance.name}
+        student age  : {curr_instance.age}
+        student gender : {curr_instance.gender}
+        student mobile : {curr_instance.mobile}
+        student email : {curr_instance.email}
+        student course : {curr_instance.course}
+        student marks : {curr_instance.marks}
+        college name : {curr_instance.college_name}
+        ''')
+
+
+
+
 def display_option():
     print('''
     -------------------------------------------------
     |        Display Menu                           |
     -------------------------------------------------
-    |        1. Add Student                         |
-    |        2. View All Students                   |
-    |        3. Search Student                      |
-    |        4. Update Student                      |
-    |        5. Delete Student                      |
-    |        6. Save to File (Optional)             |
-    |        7. Load from File (Optional)           |
-    |        8. Exit                                |
-    -------------------------------------------------
+    | ========= STUDENT RECORD SYSTEM ==============|
+    |                                               |      
+    |            1. Add Student                     |
+    |            2. View All Students               |
+    |            3. Search Student                  |
+    |            4. Update Student                  |
+    |            5. Delete Student                  |
+    |            6. Show Passed Students            |
+    |            7. Show Failed Students            |
+    |            8. Show Topper                     |
+    |            9. Average Marks                   |
+    |            10. Save Data                      |
+    |            11. Load Data                      |
+    |            12. Exit                           |
+    |-----------------------------------------------|
     ''')
 
 def view_student():
@@ -60,19 +89,36 @@ def add_student(std_data):
             Student Id : {std_data[0]}
             Student Name : {std_data[1]}
             Student Age : {std_data[2]}
-            Student Marks : {std_data[3]}
-            Student mobile no. : {std_data[4]}''')
-        if std_data[1] in students:
+            Student gender : {std_data[3]}
+            Student mobile no. : {std_data[4]}
+            Student email : {std_data[5]}
+            Student course : {std_data[6]}
+            Student Marks : {std_data[7]}
+            ''')
+        if std_data[0] in students:
             print("You're record is already existed !")
         else:
             print("We found that you're not registered, we are creating your account :)")
             students[std_data[0]] = {
                 "name": std_data[1],
                 "age": std_data[2],
-                "marks": std_data[3],
-                "mobile no": std_data[4]
+                "student gender": std_data[3],
+                "mobile no": std_data[4],
+                "email": std_data[5],
+                "course": std_data[6],
+                "marks": std_data[7],
             }
             print("we created your account")
+            # print(f'''
+            #             Student Id : {students[0][0]}
+            #             Student Name : {students[0][1]}
+            #             Student Age : {students[0][2]}
+            #             Student gender : {students[0][3]}
+            #             Student mobile no. : {students[0][4]}
+            #             Student email : {students[0][5]}
+            #             Student course : {students[0][6]}
+            #             Student Marks : {students[0][7]}
+            #             ''')
             print(students)
             # display_option()
 
@@ -83,8 +129,12 @@ def std_input():
     except:
         print("please enter the valid name !")
     stdAge = int(input("Enter your Age: "))
-    stdMarks = int(input("Enter your Marks: "))
+    stdgender = str(input("Enter your gender: "))
     stdmobile = input("Enter your mobile number: ")
+    stdemail = input("Enter your email : ")
+    stdCourse = input("Enter your course: ")
+    stdMarks = int(input("Enter your Marks: "))
 
-    res :list =[stdId,stdName,stdAge,stdMarks,stdmobile]
+
+    res :list =[stdId,stdName,stdAge,stdgender,stdmobile,stdemail,stdCourse,stdMarks]
     return res
